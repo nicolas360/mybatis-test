e14d36e9c0609f30107f023d618e905065135878
nicolas360 <654460950@qq.com>
2015-03-19 09:54:48 +0800

    first commit

---
+++ src/main/java/com/zhuc/mybatis/service/CountryService.java
@@ -0,0 +1,62 @@
+package com.zhuc.mybatis.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zhuc.mybatis.entity.Country;
+import com.zhuc.mybatis.entity.CountryExample;
+import com.zhuc.mybatis.mapper.CountryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by ZHUC on 2015/3/18.
+ */
+@Service
+@Transactional
+public class CountryService {
+
+    @Autowired
+    private CountryMapper countryMapper;
+
+    public Country findById(Integer id){
+        return countryMapper.selectByPrimaryKey(id);
+    }
+
+    public void delete(Integer id){
+        countryMapper.deleteByPrimaryKey(id);
+        throw new RuntimeException("123123"); //有事务可回滚
+    }
+
+    public List<Country> select(){
+        CountryExample example = new CountryExample();
+        example.createCriteria().andCountryCodeEqualTo("CL");
+
+        return countryMapper.selectByExample(example);
+    }
+
+    public void insert(Country c){
+        countryMapper.insert(c);
+    }
+
+    public void update(Country c){
+        countryMapper.updateByPrimaryKey(c);
+    }
+
+    public PageInfo<Country> page2(Integer pageNum, Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        CountryExample example = new CountryExample();
+        example.setOrderByClause("country_Name desc");
+        List<Country> list = countryMapper.selectByExample(example);
+        return new PageInfo(list);
+    }
+
+    public List<Country> page(Integer pageNum, Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        CountryExample example = new CountryExample();
+        example.setOrderByClause("country_Name desc");
+        return countryMapper.selectByExample(example);
+    }
+}
---
+++ src/test/java/com/zhuc/Test1.java
@@ -0,0 +1,63 @@
+package com.zhuc;
+
+import com.github.pagehelper.PageInfo;
+import com.zhuc.mybatis.service.CountryService;
+import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.fastjson.JSON;
+import com.zhuc.mybatis.entity.Country;
+import com.zhuc.mybatis.mapper.CountryMapper;
+
+import java.util.List;
+
+/**
+ * Created by ZHUC on 2015/3/18.
+ */
+public class Test1 extends BasicTest {
+
+//	@Autowired
+//	private SqlSessionTemplate sqlSessionTemplate;
+//
+//	@Autowired
+//	private CountryMapper countryMapper;
+
+	@Autowired
+	private CountryService countryService;
+
+	@Test
+	public void service(){
+//		countryService.delete(35);
+
+		Country c = countryService.findById(34);
+		System.out.println(JSON.toJSONString(c));
+
+		List<Country> list = countryService.select();
+		System.out.println(JSON.toJSONString(list));
+
+		Country cc = new Country();
+		cc.setCountryCode("CN");
+		cc.setCountryName("China");
+//		countryService.insert(cc);
+
+
+		List<Country> page = countryService.page(1, 20);
+		System.out.println(JSON.toJSONString(page));
+
+		PageInfo<Country> page2 = countryService.page2(2, 20);
+		System.out.println(JSON.toJSONString(page2));
+
+	}
+
+//	@Test
+//	public void sql() {
+//		CountryMapper cm = sqlSessionTemplate.getMapper(CountryMapper.class);
+//		Country c = cm.selectByPrimaryKey(35);
+//		System.out.println(JSON.toJSONString(c));
+//
+//		Country c2 = countryMapper.selectByPrimaryKey(35);
+//		System.out.println(JSON.toJSONString(c2));
+//	}
+
+}
